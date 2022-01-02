7b5cc0aea760c07b5496c04dfe91b29adbb96f81
Jongim Jeon <menthamin@naver.com>
2022-01-02 22:38:40 +0900

    [feat] add 객체지향: 다형성 및 캡슐화

--- 4.object/코드로알아보는객체지향특성.py
+++ 4.object/코드로알아보는객체지향특성.py
@@ -7,12 +7,15 @@
         1. 상속
         2. 추상화
         3. 다형성
-        4. 캡슐화
+        4. 캡슐화: 객체 내부의 데이터나 메서드의 구체적인 로직을 외부에서 모르고 사용해도 문제가 없도록 하는 특성
     
     객체지향의 설계 5원칙 (SOLID)
         1. SRP(Single Responsibility Principle)
             - 하나의 객체는 하나의 책임만 가지도록 설계하는 것이 일반적으로 좋다.
-    
+        2. OCP(Open-Close Principle)
+            - 소프트웨어는 확장에 대해 열려 있어야 하고, 수정에 대해서는 닫혀 있어야 한다는 원칙입니다.
+            - 쉽게 말해, 요구사항이 바뀌어 기존 코드를 변경해야 할 때, 기존 코드를 수정하지 않고 새로운 코드를
+              추가하는 것이 좋다는 것입니다.
     책임주도 설계: 객체들의 인스턴스, 메써드보다 객체들의 책임부터 정의하고 시스템을 설계한다.
 """
 
@@ -138,6 +141,7 @@ class User:
         self.car.accelerate()
 
 
+# 다형성
 print("추상화 적용")
 
 # 모닝 차량을 운전하고 싶을 때
@@ -149,3 +153,94 @@ print(f"모닝 속도: {user.car.speed}")
 user = User(PorscheCar())
 user.drive()
 print(f"포르쉐 속도: {user.car.speed}")
+
+# 캡슐화
+# - 객체 내부의 데이터나 메서드의 구체적인 로직을 외부에서 모르고 사용해도 문제가 없도록 하는 특성
+print("캡술화 예제: 캡슐화하지 않을 때")
+
+
+class MorningCar:
+    def __init__(self, fuel: int) -> None:
+        self.speed = 0
+        self._current_fuel = fuel
+        self.max_fuel = 50
+        if fuel > self.max_fuel:
+            raise Exception(f"최대로 넣을 수 있는 기름은 {self.max_fuel}L 입니다")
+
+    def accelerate(self) -> None:
+        if self._current_fuel == 0:
+            raise Exception("남아있는 기름이 없습니다")
+        self.speed += 1
+        self._current_fuel -= 1
+
+    def decelerate(self) -> None:
+        if self._current_fuel == 0:
+            raise Exception("남아있는 기름이 없습니다")
+        self.speed -= 1
+        self._current_fuel -= 1
+
+
+# 차량 생성
+car = MorningCar(50)
+
+for i in range(45):
+    print(f"현재 속도는 {car.speed}입니다.")
+    car.accelerate()
+
+car._current_fuel += 50
+print(car._current_fuel)
+
+print(f"남은 기름은 {round(car._current_fuel / car.max_fuel * 100, 1)} % 입니다.")
+
+# 캡슐화
+print("캡술화 예제: 캡슐화 했을 때")
+
+
+class MorningCar:
+    def __init__(self, fuel: int) -> None:
+        self.speed = 0
+        self._current_fuel = fuel  # 비공개형 변수로 바꿉니다.
+        self._max_fuel = 50  # 비공개형 변수로 바꿉니다.
+        if fuel > self._max_fuel:
+            raise Exception(f"최대로 넣을 수 있는 기름은 {self._max_fuel}L 입니다!")
+
+    def accelerate(self) -> None:
+        if self._current_fuel == 0:
+            raise Exception("남아있는 기름이 없습니다!")
+        self.speed += 1
+        self._current_fuel -= 1
+
+    def decelerate(self) -> None:
+        if self._current_fuel == 0:
+            raise Exception("남아있는 기름이 없습니다!")
+        self.speed -= 1
+        self._current_fuel -= 1
+
+    # 차량에 주유할 수 있는 기능을 메서드로 제공하고, 구체적인 로직은 객체 외부에서 몰라도 되도록 메서드 내에 둡니다.
+    def refuel(self, fuel: int) -> None:
+        if self._current_fuel + fuel > self._max_fuel:
+            raise Exception(
+                f"추가로 더 넣을 수 있는 최대 연료는 {self._max_fuel - self._current_fuel}L 입니다!"
+            )
+        self._current_fuel += fuel
+
+    # 차량의 남은 주유랑을 퍼센트로 확인하는 기능을 메서드로 제공합니다.
+    def get_current_fuel_percentage(self) -> str:
+        return f"{self._current_fuel / self._max_fuel * 100} %"
+
+
+car = MorningCar(50)
+
+for i in range(45):
+    print(f"현재 속도는 {car.speed}입니다.")
+    car.accelerate()
+    print(car.get_current_fuel_percentage())
+
+try:
+    car.refuel(50)
+except Exception as e:
+    print(e)
+
+car.refuel(30)
+
+print(car.get_current_fuel_percentage())
